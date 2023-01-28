731b2cd8e3da61fb35b0922f48dd7fc320733cc0
Salomon Chambi <salomonchambi@gmail.com>
2023-01-28 18:21:58 -0500

    feat: use immediately invoked function expression aka IIFE

---
+++ fundamentals/src/03-iife.ts
@@ -0,0 +1,11 @@
+/*
+  understanding Immediately Invoked Function Expression IIFE for short
+  also known as Self-Executing Anonymous Function (Funcion anonima auto-ejecutada)
+  This is useful to use variable names in different ts files
+  */
+(()=> {
+  let productName = 'Product 1'; // inferred type: string
+  let productPrice = 123; // inferred type: number
+})();
+// if those lines where outside the IIFE, the static analysis will throw an error
+// because those variables are being used in another file (03-typing.ts)
