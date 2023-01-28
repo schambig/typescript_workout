ba4a2ee99894c93f757d5ebb65a6a7975b75bfe2
Salomon Chambi <salomonchambi@gmail.com>
2023-01-28 18:04:26 -0500

    feat: practice with inferred types

---
+++ fundamentals/src/03-typing.ts
@@ -0,0 +1,10 @@
+let productName = 'Product 1'; // inferred type: string
+let productPrice = 123; // inferred type: number
+
+productName = 'Product 2'; // as long as it is a string it can be changed
+productName.toLocaleLowerCase(); // different methods for each value type
+
+productPrice.toFixed(); // different methods for each value type
+
+const productStock = 1000; // cannot be reassigned to refference a different value
+const productName2 = 'Product 2'; // same of above
