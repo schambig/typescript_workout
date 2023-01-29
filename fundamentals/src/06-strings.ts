17374f6a785bc92ecf1756218230a7701e4c0b86
Salomon Chambi <salomonchambi@gmail.com>
2023-01-28 23:53:46 -0500

    feat: practice with string value

---
+++ fundamentals/src/06-strings.ts
@@ -0,0 +1,26 @@
+(()=>{
+  let productTitle = 'My amazing product'; // inferred value: string
+  // productTitle = null;
+  // productTitle = () => {};
+  // productTitle = 123;
+  productTitle = 'My amazing product changed';
+  console.log('productTitle', productTitle);
+
+  const productDescription = "I'm bla bla ba bla bl s asasas"; // (" ' "), (' " '), (\')
+  console.log('productDescription', productDescription);
+
+  let productPrice = 100;
+  let isNew: boolean = false; // using Type Annotation
+
+   // using Template literals (Template strings)
+  const summary = `
+    title: ${productTitle}
+    description: ${productDescription}
+    price: ${productPrice}
+    isNew: ${isNew}
+  `;
+
+  console.log(summary);
+
+  const myString: string = ''; // String =/= string
+})();
