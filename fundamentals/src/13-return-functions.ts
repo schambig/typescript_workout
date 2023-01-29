53e6fe803d9c7efbafcf3915fab510420099e401
Salomon Chambi <salomonchambi@gmail.com>
2023-01-29 13:09:09 -0500

    feat: practice with return of functions

---
+++ fundamentals/src/13-return-functions.ts
@@ -0,0 +1,18 @@
+(()=>{
+  // use Type Annotation for parameter and return value, do not infer
+  const calcTotal = (prices: number[]): string => {
+    let total = 0;
+    prices.forEach((item)=> {
+      total += item;
+    });
+    return total.toString(); // use toString() method because 'total' is number value
+  }
+
+  // use Type Annotation (: void) for the return of the function
+  const printTotal = (prices: number[]): void => {
+    const rta = calcTotal(prices);
+    console.log(`El total es ${rta}`);
+  }
+
+  printTotal([1,2,1,1,1]);
+})();
