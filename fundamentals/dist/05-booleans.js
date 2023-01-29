d9d32599c33e82ec587ff08aa19356490ef3fa3e
Salomon Chambi <salomonchambi@gmail.com>
2023-01-28 23:47:08 -0500

    feat: add transpiled file

---
+++ fundamentals/dist/05-booleans.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let isEnable = true; // inferred value: boolean
+    //  isEnable = 'as';
+    //  isEnable = 1212;
+    isEnable = false;
+    let isNew = false; // using Type Annotation, expicit
+    console.log('isNew', isNew);
+    isNew = true;
+    console.log('isNew', isNew);
+    const random = Math.random(); // this gives us a number between 0 and 1
+    console.log('random', random);
+    isNew = random >= 0.5 ? true : false; // ternary, we also can use 'random>=0.5'
+    console.log('isNew', isNew);
+    const myBoolean = true;
+})();
