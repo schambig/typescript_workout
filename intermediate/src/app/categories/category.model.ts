b1d0647c4f8711bfd3cdf0e00c05ef195f5e2ccd
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 01:21:12 -0500

    feat: import basemodel and extend an interface

--- intermediate/src/app/categories/category.model.ts
+++ intermediate/src/app/categories/category.model.ts
@@ -1,6 +1,8 @@
+import { BaseModel } from "../base.model";
+
 // we will use this interface as a new property of Product interface in ../products/product.model.ts
-export interface Category {
-  id: string | number;
+export interface Category extends BaseModel{
+  // id: string | number; add this property through an extend using BaseModel
   name: string;
 }
 
