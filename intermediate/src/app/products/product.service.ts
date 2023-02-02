60086b91ac970fbeeaae8c393774f93349452a09
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 14:28:53 -0500

    feat: try to overwrite readonly variables

--- intermediate/src/app/products/product.service.ts
+++ intermediate/src/app/products/product.service.ts
@@ -3,6 +3,8 @@ import { Product } from './product.model';
 const products: Product[] = [];
 
 const addProduct = (data: Product) => {
+  // data.id = 'new id'; use of readonly keyword prevent a variable to be overwritten
+  // data.createdAt = new Date(1998, 1, 1); use of readonly keyword prevent a variable to be overwritten
   products.push(data);
 }
 
