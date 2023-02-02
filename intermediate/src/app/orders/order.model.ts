eaea43d233a1e72f1a6f7c545543ca231530ee21
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 01:21:33 -0500

    feat: import basemodel and extend an interface

--- intermediate/src/app/orders/order.model.ts
+++ intermediate/src/app/orders/order.model.ts
@@ -1,9 +1,10 @@
+import { BaseModel } from "../base.model";
 import { Product } from "../products/product.model";
 import { User } from "../users/user.model";
 
-export interface Order {
-  id: string | number;
-  createdAt: Date;
+export interface Order extends BaseModel{
+  // id: string | number; add this property through an extend using BaseModel
+  // createdAt: Date; add this property through an extend using BaseModels
   products: Product[];
   user: User;
 }
