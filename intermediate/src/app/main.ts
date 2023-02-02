da9aa723003c96e6c62de86415eccbf113de21a5
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 01:24:49 -0500

    feat: add new properties in category sub-object

--- intermediate/src/app/main.ts
+++ intermediate/src/app/main.ts
@@ -4,9 +4,12 @@ addProduct({
   id: '1',
   title: 'p1',
   createdAt: new Date(),
+  updatedAt: new Date(), // add this propertie due to the extends
   stock: 90,
   category: { // this new category wad added due to the change of product.model.ts
     id: 12,
-    name: 'c1'
+    name: 'c1',
+    createdAt: new Date(), // add these two properties
+    updatedAt: new Date(), // due to the extends
   }
 });
