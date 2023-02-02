06e0dcfce45c3e4e6904d98a78d1be232444542e
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 01:21:57 -0500

    feat: import basemodel and extend an interface

--- intermediate/src/app/users/user.model.ts
+++ intermediate/src/app/users/user.model.ts
@@ -1,11 +1,13 @@
+import { BaseModel } from "../base.model";
+
 enum ROLES {
   ADMIN = "admin",
   SELLER = "seller",
   CUSTOMER = "customer",
 }
 
-interface User {
-  id: string | number;
+interface User extends BaseModel{
+  // id: string | number; add this property through an extend using BaseModel
   username: string;
   role: ROLES; // use enum as Type Annotation
 }
