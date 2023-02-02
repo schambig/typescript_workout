b9775f8ebbea4b3d0255ac0cb1a191b2ffcd475f
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 14:27:51 -0500

    feat: make the variables read only, prevent future overwrite

--- intermediate/src/app/base.model.ts
+++ intermediate/src/app/base.model.ts
@@ -1,7 +1,7 @@
 // create a new interface to extend the properties of another one
 // we will extend the interfaces located in all *.model.ts files (04)
 export interface BaseModel {
-  id: string | number;
-  createdAt: Date;
+  readonly id: string | number; // readonly prevent a variable to be overwritten\
+  readonly createdAt: Date; // making it read only
   updatedAt: Date;
 }
