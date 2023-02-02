84860400ebdf76b07d571b82e49a5f4496342a63
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 14:53:35 -0500

    feat: add readonly keyword definition in comments

--- intermediate/src/app/base.model.ts
+++ intermediate/src/app/base.model.ts
@@ -5,3 +5,24 @@ export interface BaseModel {
   readonly createdAt: Date; // making it read only
   updatedAt: Date;
 }
+
+/*
+  The readonly keyword in TypeScript is used to declare a property as read-only.
+  This means that the property can only be read and cannot be modified after its initial assignment.
+  ¡¡**The readonly keyword can be applied to properties of classes, interfaces, and objects.**!!
+
+    class Person {
+      readonly name: string;
+
+      constructor(name: string) {
+        this.name = name;
+      }
+    }
+
+    const person = new Person('John Doe');
+    person.name = 'Jane Doe'; // Error: Cannot assign to 'name' because it is a read-only property.
+
+  By declaring properties as readonly, you can help ensure that the state of your objects remains consistent
+  and that unintended changes are prevented. It also helps to make your code more self-documenting
+  and can improve the maintainability and readability of your code.
+*/
