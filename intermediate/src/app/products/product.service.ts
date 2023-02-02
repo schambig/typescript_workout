ebaeaec4ef311109fa337c3accf5f1e9e030fb6a
Salomon Chambi <salomonchambi@gmail.com>
2023-02-02 00:32:38 -0500

    feat: create addProduct function

---
+++ intermediate/src/app/products/product.service.ts
@@ -0,0 +1,12 @@
+import { Product } from './product.model';
+
+const products: Product[] = [];
+
+const addProduct = (data: Product) => {
+  products.push(data);
+}
+
+export {
+  products,
+  addProduct
+}
