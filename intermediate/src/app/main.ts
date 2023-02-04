c31a659618602a2877275636822e5c70f0e31192
Salomon Chambi <salomonchambi@gmail.com>
2023-02-04 15:06:10 -0500

    feat: delete properties added by CreateProductDto

--- intermediate/src/app/main.ts
+++ intermediate/src/app/main.ts
@@ -3,7 +3,6 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.datatype.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.imageUrl(),
     color: faker.color.human(),
@@ -12,15 +11,8 @@ for (let index = 0; index < 50; index++) {
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements(),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(), // add this propertie due to the extends
     stock: faker.datatype.number({min: 10, max:100}),
-    category: { // this new category wad added due to the change of product.model.ts
-      id: faker.datatype.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(), // add these two properties
-      updatedAt: faker.date.recent(), // due to the extends
-    }
+    categoryId: faker.datatype.uuid()
   });
 }
 
