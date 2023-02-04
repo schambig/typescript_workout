198a4269e561b4b1adbaf32316b6d3a27b8090e6
Salomon Chambi <salomonchambi@gmail.com>
2023-02-04 03:01:32 -0500

    feat: use faker library to properties values

--- intermediate/src/app/main.ts
+++ intermediate/src/app/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
+import { addProduct, products } from './products/product.service';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(), // add this propertie due to the extends
-  stock: 90,
-  category: { // this new category wad added due to the change of product.model.ts
-    id: 12,
-    name: 'c1',
-    createdAt: new Date(), // add these two properties
-    updatedAt: new Date(), // due to the extends
-  }
-});
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(), // add this propertie due to the extends
+    stock: faker.datatype.number({min: 10, max:100}),
+    category: { // this new category wad added due to the change of product.model.ts
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(), // add these two properties
+      updatedAt: faker.date.recent(), // due to the extends
+    }
+  });
+}
+
+console.log(products);
