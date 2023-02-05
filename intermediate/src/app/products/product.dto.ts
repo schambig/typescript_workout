fa4cb8ca5d8d78340baf4ff101969c13efd38ab3
Salomon Chambi <salomonchambi@gmail.com>
2023-02-04 22:41:33 -0500

    feat: create and export UpdateProductDto interface, use Partial

--- intermediate/src/app/products/product.dto.ts
+++ intermediate/src/app/products/product.dto.ts
@@ -1,8 +1,21 @@
 import { Product } from "./product.model";
 
-export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
+interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string;
 };
 
 // opposite of Omit utility type (same implementation)
-type example = Pick <Product, 'color' | 'description'>;
+type example = Pick<Product, 'color' | 'description'>;
+
+// Partial Utility Type make all CreateProductDto properties optional
+// except for those that are omitted
+interface UpdateProductDto extends Partial<CreateProductDto> {};
+
+// opposite of Partial utility type (same implementation)
+// this utility type make all properties mandatory (obligatorias)
+type example2 = Required<Product>;
+
+export {
+  CreateProductDto,
+  UpdateProductDto
+}
