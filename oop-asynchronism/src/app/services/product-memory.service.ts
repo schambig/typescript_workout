b2ee0ec89d8228b89a8398c228f1a51e9f958723
Salomon Chambi <salomonchambi@gmail.com>
2023-02-11 11:01:23 -0500

    feat: migrate functions to a class, create new methods

---
+++ oop-asynchronism/src/app/services/product-memory.service.ts
@@ -0,0 +1,50 @@
+import { faker } from '@faker-js/faker';
+
+import { Product } from '../models/product.model';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+// import { ProductService } from '../models/product-service.model'; commit this change in next push
+
+// create a class based on previous functions, ('implements ProductService' comes in next commit an error is shown by now)
+export class ProductMemoryService implements ProductService {
+  private products: Product[] = [];
+
+  // commit this change in next push
+  // getAll() {
+  //   return this.products;
+  // }
+
+  create(data: CreateProductDto): Product {
+    const newProduct = {
+      ...data,
+      id: faker.datatype.number(),
+      category: {
+        id: data.categoryId,
+        name: faker.commerce.department(),
+        image: faker.image.imageUrl(),
+      }
+    }
+    return this.add(newProduct);
+  }
+
+  add(product: Product) {
+    this.products.push(product);
+    return product;
+  }
+
+  // add 'this' to 'products' to migrate function to class, it is not necessary tu use arrow functions
+  // compare with original function: 'intermediate/src/app/products/product.service.ts'
+  // the same with all the above code
+  update(id: Product['id'], changes: UpdateProductDto ): Product {
+    const index = this.products.findIndex(item => item.id === id);
+    const prevData = this.products[index];
+    this.products[index] = {
+      ...prevData,
+      ...changes
+    }
+    return this.products[index];
+  }
+
+  findOne(id: Product['id']) {
+    return this.products.find(item => item.id === id);
+  }
+}
